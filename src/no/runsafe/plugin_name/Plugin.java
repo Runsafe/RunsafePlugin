fb2e1ad2fca75383078a0d653b514b60b42f7115
Morten Nilsen <morten@runsafe.no>
2013-01-08 19:44:31 +0100

    Bare bones project

---
+++ src/no/runsafe/plugin_name/Plugin.java
@@ -0,0 +1,12 @@
+package no.runsafe.plugin_name;
+
+import no.runsafe.framework.RunsafePlugin;
+
+public class Plugin extends RunsafePlugin
+{
+	@Override
+	protected void PluginSetup()
+	{
+		addComponent(Component.class);
+	}
+}
