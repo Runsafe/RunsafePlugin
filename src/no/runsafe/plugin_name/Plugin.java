ab88c67d5b41f1b1745538272e1f4734c88521c8
Morten Nilsen <morten@runsafe.no>
2013-12-07 15:09:03 +0100

    Make a plugin-specific debugger easily accessible

--- src/no/runsafe/plugin_name/Plugin.java
+++ src/no/runsafe/plugin_name/Plugin.java
@@ -1,12 +1,16 @@
 package no.runsafe.plugin_name;
 
 import no.runsafe.framework.RunsafePlugin;
+import no.runsafe.framework.api.IDebug;
 
 public class Plugin extends RunsafePlugin
 {
+	public static IDebug Debugger = null;
+
 	@Override
 	protected void PluginSetup()
 	{
+		Debugger = getComponent(IDebug.class);
 		//addComponent(SomeComponent.class);
 	}
 }
