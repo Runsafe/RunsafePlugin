d99aa2ba3b5023d52f4dc5b594d3cae23fe60476
Morten Nilsen <morten@runsafe.no>
2013-12-15 13:05:36 +0100

    Extend sample plugin

--- src/no/runsafe/plugin_name/Plugin.java
+++ src/no/runsafe/plugin_name/Plugin.java
@@ -1,16 +1,26 @@
 package no.runsafe.plugin_name;
 
 import no.runsafe.framework.RunsafePlugin;
-import no.runsafe.framework.api.log.IDebug;
 
 public class Plugin extends RunsafePlugin
+//public class Plugin extends RunsafeConfigurablePlugin
 {
-	public static IDebug Debugger = null;
+//	public static IDebug Debugger = null;
 
 	@Override
 	protected void PluginSetup()
 	{
-		Debugger = getComponent(IDebug.class);
-		//addComponent(SomeComponent.class);
+//		Debugger = getComponent(IDebug.class);
+
+		// Framework features
+//		addComponent(Commands.class);
+//		addComponent(Database.class);
+//		addComponent(Events.class);
+//		addComponent(FrameworkHooks.class);
+//		addComponent(LUAScripts.class);
+//		addComponent(UniverseRegistration.class);
+
+		// Plugin components
+//		addComponent(SomeComponent.class);
 	}
 }
