326993d604f006e7f2f99049e3138307603e0b7d
Kruithne <kruithne@gmail.com>
2013-12-22 22:21:32 +0000

    When we create the plug-in, we should have the entry point named the same as the plug-in.

---
+++ src/no/runsafe/plugin_name/PluginName.java
@@ -0,0 +1,26 @@
+package no.runsafe.plugin_name;
+
+import no.runsafe.framework.RunsafePlugin;
+
+public class PluginName extends RunsafePlugin
+//public class PluginName extends RunsafeConfigurablePlugin
+{
+//	public static IDebug Debugger = null;
+
+	@Override
+	protected void pluginSetup()
+	{
+//		Debugger = getComponent(IDebug.class);
+
+		// Framework features
+//		addComponent(Commands.class);
+//		addComponent(Database.class);
+//		addComponent(Events.class);
+//		addComponent(FrameworkHooks.class);
+//		addComponent(LUAScripts.class);
+//		addComponent(UniverseRegistration.class);
+
+		// Plugin components
+//		addComponent(SomeComponent.class);
+	}
+}
